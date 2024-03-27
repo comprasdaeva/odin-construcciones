44a2d44ab1a63fda7e36007873ead7a36492ee92
CAROBIAS <carolinainesdelgado@gmail.com>
2024-03-26 22:26:00 -0300

    menu grua y  responsive
    .

--- script.js
+++ script.js
@@ -97,3 +97,17 @@ function initCounterOnScroll() {
 initCounterOnScroll();
 
 
+//menu hamburguesa
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuGrua = document.getElementById('menu-grua');
+    const gruaImg = menuGrua.querySelector('img');
+    const gruaCheckbox = menuGrua.querySelector('.grua');
+
+    gruaImg.addEventListener('click', function() {
+        gruaImg.classList.add('shake');
+        setTimeout(() => {
+            gruaImg.classList.remove('shake');
+        }, 300);
+    });
+});
